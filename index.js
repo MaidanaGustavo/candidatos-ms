60ef3a66137de81a50ecd2ba2dd07c59fb75b8f8
MaidanaGustavo <mr.xibisnet@gmail.com>
2020-11-21 22:41:06 -0400

    percorendo

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
 const fs = require('fs')
-
-fs.readFile("./candidatos/120000633198.json","utf8",function(err,data){
-    if(err){
-        return console.log("erro ao ler arquivo")
-    }
-    const jsonData = JSON.parse(data)
-
-   // console.log(jsonData)
+const graceful = require('graceful-fs')
+graceful.gracefulify(fs)
+fs.readdir('./candidatos',function(error,files){
+	exibe(files)
 });
+
+const exibe = function (obj){
+	for(let i = 0 ; i<obj.length;i++){
+		lerArquivo('./candidatos/'+obj[i])
+	}
+}
+
+const lerArquivo  = function(str){
+	let obj
+	fs.readFile(str,"utf8",function(err,data){
+    if(err){
+        
+    }
+     const jsonData = JSON.parse(data)
+     pegarDado(jsonData)
+});
+	
+}
+
+const pegarDado = function(data){
+	percorreBens(data.bens)
+	percorreArquivos(data.arquivos)
+	percorreEleicao(data.eleicoesAnteriores)
+}
+
+const percorreBens = function(arrayBens){
+	for(let i = 0;i<arrayBens.length;i++){
+		console.log(arrayBens[i].ordem)
+	}
+}
+
+const percorreArquivos = function(arquivos){
+	for(let i = 0;i<arquivos.length;i++){
+		console.log(arquivos[i].idArquivo)
+	}
+}
+
+const percorreEleicao = function(eleicaoAnterior){
+	for(let i = 0;i<eleicaoAnterior.length;i++){
+		console.log(eleicaoAnterior[i].id)
+	}
+}
 
---
+++ insertBens.js
@@ -0,0 +1,13 @@
+const insersaoBens  = function(array,index){
+
+	if(index === array.length- 1){
+		return true
+	}else{
+		console.log(array[i])
+		return insersaoBens(array,index+1)
+	}
+
+	
+}
+
+module.exports.insersao = insersaoBens();
